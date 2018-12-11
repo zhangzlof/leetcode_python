292218b14aec8222678199a1435f9f2fa9c9fe67
zhangzlof <43635266+zhangzlof@users.noreply.github.com>
2018-12-11 10:03:03 +0800

    search in rotated sorted array

---
+++ 33. Search in Rotated Sorted Array/Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start+end) // 2
+            if (nums[mid] == target):
+                return mid
+            if nums[mid] >= nums[start]:    #mid在左升序部分
+                if (target>= nums[start] and target<= nums[mid]):
+                    end = mid - 1
+                else:
+                    start = mid + 1 
+            if nums[mid] <= nums[end]:   #mid在右升序部分
+                if (target >= nums[mid] and target <= nums[end]):
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
